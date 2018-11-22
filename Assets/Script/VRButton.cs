4286468d722bf6dc95dfbb31f9fbd9756a1a142a
BlueDove <bluedulgi1104@gmail.com>
2018-11-22 17:55:53 +0900

    VR수업 2차_카메라와이벤트

---
+++ Assets/Script/VRButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRButton : MonoBehaviour {
+
+    #region Variables
+
+    
+
+    #endregion
+
+    #region Other Methods
+
+    public void ButtonClicked()
+    {
+        Destroy(this.gameObject);
+    }
+
+    #endregion
+}
---
+++ Assets/Script/VRCamera.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRCamera : MonoBehaviour {
+
+    #region Variables
+
+    public Text TimeText;
+
+    public float BreakTime = 3f;
+
+    private float eventTime = 0;
+    private bool eventOn = false;
+
+    private GameObject EventObj;
+
+    #endregion
+
+    #region Components
+
+    private Transform tr;
+    private Ray ray;
+    private RaycastHit hit;
+
+    #endregion
+
+    #region LifeCycle Methods
+
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        ray = new Ray(tr.position, tr.forward * 100f);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.CompareTag("BreakAble"))
+            {
+                //부서지게
+                eventOn = true;
+                EventObj = hit.collider.gameObject;
+            }
+            else if(hit.collider.CompareTag("VRButton"))
+            {
+                eventOn = true;
+                EventObj = hit.collider.gameObject;
+            }
+            else
+            {
+                eventOn = false;
+                EventObj = null;
+                eventTime = 0;
+            }
+        }
+        else
+        {
+            eventOn = false;
+            EventObj = null;
+            eventTime = 0;
+        }
+
+        if(eventOn)
+        {
+            eventTime += Time.deltaTime;
+
+            if(eventTime > BreakTime)
+            {
+                if (EventObj.CompareTag("BreakAble"))
+                {
+                    Destroy(EventObj);
+                } else if(EventObj.CompareTag("VRButton"))
+                {
+                    EventObj.GetComponent<VRButton>().ButtonClicked();
+                    EventObj = null;
+                }
+                eventOn = false;
+                eventTime = 0;
+            }
+        }
+
+        TimeText.text = ((int)eventTime).ToString();
+    }
+
+    #endregion
+
+    #region Other Methods
+
+
+
+    #endregion
+}
